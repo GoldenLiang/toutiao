a9fb291e7d034195749b2ccc58776644feb5ab92
goldenLiang <870385737@qq.com>
2018-02-02 23:04:42 +0800

    Like

---
+++ src/main/java/com/lc/controller/LikeController.java
@@ -0,0 +1,57 @@
+package com.lc.controller;
+
+import com.lc.async.EventModel;
+import com.lc.async.EventProducer;
+import com.lc.async.EventType;
+import com.lc.model.EntityType;
+import com.lc.model.HostHolder;
+import com.lc.model.News;
+import com.lc.service.LikeService;
+import com.lc.service.NewsService;
+import com.lc.util.ToutiaoUtil;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by lc on 2017/7/13.
+ */
+@Controller
+public class LikeController {
+    @Autowired
+    LikeService likeService;
+
+    @Autowired
+    HostHolder hostHolder;
+
+    @Autowired
+    NewsService newsService;
+
+    @Autowired
+    EventProducer eventProducer;
+
+    @RequestMapping(path = {"/like"}, method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String like(@Param("newId") int newsId) {
+        long likeCount = likeService.like(hostHolder.getUser().getId(), EntityType.ENTITY_NEWS, newsId);
+        // 更新喜欢数
+        News news = newsService.getById(newsId);
+        newsService.updateLikeCount(newsId, (int) likeCount);
+        eventProducer.fireEvent(new EventModel(EventType.LIKE)
+                .setEntityOwnerId(news.getUserId())
+                .setActorId(hostHolder.getUser().getId()).setEntityId(newsId));
+        return ToutiaoUtil.getJSONString(0, String.valueOf(likeCount));
+    }
+
+    @RequestMapping(path = {"/dislike"}, method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String dislike(@Param("newId") int newsId) {
+        long likeCount = likeService.disLike(hostHolder.getUser().getId(), EntityType.ENTITY_NEWS, newsId);
+        // 更新喜欢数
+        newsService.updateLikeCount(newsId, (int) likeCount);
+        return ToutiaoUtil.getJSONString(0, String.valueOf(likeCount));
+    }
+}
