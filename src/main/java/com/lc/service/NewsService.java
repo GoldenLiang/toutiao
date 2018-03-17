5de29e387fe6ae025d61f25ce7c3314c35c407cb
goldenLiang <870385737@qq.com>
2018-03-17 22:19:57 +0800

    debug

--- src/main/java/com/lc/service/NewsService.java
+++ src/main/java/com/lc/service/NewsService.java
@@ -58,7 +58,7 @@ public class NewsService {
     
     public void setNewsScore(News news) {  
     	double score = rankingNewsAlgorithm(news);
-    	jedisAdapter.zadd(getDateKey(0), score, String.valueOf(news.getId()));
+    	jedisAdapter.zadd(getDateKey(getTimeDistance(news.getCreatedDate(), new Date())), score, String.valueOf(news.getId()));
     }
     
     public void deleteNewsScore(int newsId) {  
@@ -88,6 +88,23 @@ public class NewsService {
 		}
 		return news;
 	}
+    
+    public int getTimeDistance(Date beginDate , Date endDate) {  
+        Calendar beginCalendar = Calendar.getInstance();  
+        beginCalendar.setTime(beginDate);  
+        Calendar endCalendar = Calendar.getInstance();  
+        endCalendar.setTime(endDate);  
+        long beginTime = beginCalendar.getTime().getTime();  
+        long endTime = endCalendar.getTime().getTime();  
+        int betweenDays = (int)((endTime - beginTime) / (1000 * 60 * 60 *24));//先算出两时间的毫秒数之差大于一天的天数  
+          
+        endCalendar.add(Calendar.DAY_OF_MONTH, -betweenDays);//使endCalendar减去这些天数，将问题转换为两时间的毫秒数之差不足一天的情况  
+        endCalendar.add(Calendar.DAY_OF_MONTH, -1);//再使endCalendar减去1天  
+        if(beginCalendar.get(Calendar.DAY_OF_MONTH)==endCalendar.get(Calendar.DAY_OF_MONTH))//比较两日期的DAY_OF_MONTH是否相等  
+            return betweenDays + 1; //相等说明确实跨天了  
+        else  
+            return betweenDays + 0; //不相等说明确实未跨天  
+    }  
 //   public synchronized List<News> rankingNews(List<News> newsList) {  
 //    	List<News> resList =  JSONObject.parseArray(jedisAdapter.get("newsList"), News.class);
 //    	if(resList == null) {
